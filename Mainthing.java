31844de55b62be41afa6c5816382dad41d939356
helpingnstuff915 <282068057+helpingnstuff915@users.noreply.github.com>
2026-05-05 21:35:55 -0400

    Changed Type function to include colours via ansi escape codes. Now includes Red, Green, and Blue. I'm not sure of the colour mapping, but im going to add them to differentiate between the player, the other player, and the AI (Single player vs computer).

--- Mainthing.java
+++ Mainthing.java
@@ -2,12 +2,29 @@
 
 public class Mainthing {
     public static void main(String[] args) {//I keep forgetting how to init, i js copy paste
-        type("Hello, Java!", 100);
-        clearScreen();
+        type("Hello, Java!", 100, "Red");
     }
 
     //func to make it typed out slowly
-    public static void type(String text, int speed) {
+    public static void type(String text, int speed, String colour) {
+        final String Red = "\u001B[31m";
+        final String Green = "\u001B[32m";
+        final String Blue = "\u001B[34m";
+        switch (colour) {
+            case "Red":
+                colour = Red;
+                break;
+            case "Green":
+                colour = Green;
+                break;
+            case "Blue":
+                colour = Blue;
+                break;
+            default:
+                // No color
+                break;
+        }
+
         for (char c : text.toCharArray()) {
             try {
                 Thread.sleep(speed);
@@ -16,14 +33,15 @@ public class Mainthing {
                 Thread.currentThread().interrupt();
                 return;
             }
-            System.out.print(c);
+            System.out.print(""+colour + c+ "");
         }
         System.out.println();
     }
 
     //Clears the console for a new game
     public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
+        System.out.print("\033[H\033[2J");//this is some cool stuff, Ansi escape codes
+        //Were also going to use ansi stuff for colors
         System.out.flush();
     }
 }
